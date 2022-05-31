255f5d5831f0189bc31f170e190e140d1dcf0f2f
hannsgh <hannsgh@bk.ru>
2022-05-31 17:37:11 +0400

    add eureka-server

--- eureka-server/src/main/java/ru/hannsgh/micro/demo/eurekaserver/EurekaServerApplication.java
+++ eureka-server/src/main/java/ru/hannsgh/micro/demo/eurekaserver/EurekaServerApplication.java
@@ -1,9 +1,11 @@
-package ru.hanns.micro.demo.eurekaserver;
+package ru.hannsgh.micro.demo.eurekaserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class EurekaServerApplication {
 
 	public static void main(String[] args) {
--- eureka-server/src/test/java/ru/hannsgh/micro/demo/eurekaserver/EurekaServerApplicationTests.java
+++ eureka-server/src/test/java/ru/hannsgh/micro/demo/eurekaserver/EurekaServerApplicationTests.java
@@ -1,4 +1,4 @@
-package ru.hanns.micro.demo.eurekaserver;
+package ru.hannsgh.micro.demo.eurekaserver;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
