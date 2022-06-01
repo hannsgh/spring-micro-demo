314142257f32b6f1a6932485c7aa39e04232d103
hannsgh <hannsgh@bk.ru>
2022-06-01 11:16:11 +0400

    add routes for eclient2

--- eureka-client2/src/main/java/ru/hannsgh/eurekaclient2/EurekaClientApplication.java
+++ eureka-client2/src/main/java/ru/hannsgh/eurekaclient2/EurekaClientApplication.java
@@ -1,4 +1,4 @@
-package ru.hannsgh.eurekaclient;
+package ru.hannsgh.eurekaclient2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- eureka-client2/src/main/java/ru/hannsgh/eurekaclient2/controller/TestController.java
+++ eureka-client2/src/main/java/ru/hannsgh/eurekaclient2/controller/TestController.java
@@ -1,4 +1,4 @@
-package ru.hannsgh.eurekaclient.controller;
+package ru.hannsgh.eurekaclient2.controller;
 
 
 import org.springframework.web.bind.annotation.GetMapping;
